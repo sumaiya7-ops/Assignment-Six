84b21676b55f19b6700b47b7c731aabf7b1057c1
sumaiya7-ops <sumaiya7-ops@gmail.com>
2026-04-18 13:57:35 +0600

    ProductCard

--- Assignment-Six/src/components/ProductCard.jsx
+++ Assignment-Six/src/components/ProductCard.jsx
@@ -6,12 +6,9 @@ const ProductCard = ({ product, addToCart, isInCart }) => {
     popular: "bg-[#e1e7ff] text-blue-500",
     new: "bg-[#dbfce7] text-[#0a883e]",
   };
-
   return (
     <div className="bg-white rounded-2xl p-8 shadow-sm border border-gray-100 flex flex-col relative hover:shadow-md transition-all duration-300">
-
-      {/* TAG */}
-      {product.tag && (
+       {product.tag && (
         <span
           className={`absolute top-5 right-5 text-[10px] font-bold px-3 py-1 rounded-full uppercase tracking-wider ${
             tagStyles[product.tag] || "bg-gray-50 text-gray-500"
@@ -20,8 +17,6 @@ const ProductCard = ({ product, addToCart, isInCart }) => {
           {product.tag}
         </span>
       )}
-
-      {/* ICON */}
       <div className="mb-6">
         <div className="w-14 h-14 bg-[#F5F3FF] rounded-2xl flex items-center justify-center">
           <img
@@ -30,19 +25,13 @@ const ProductCard = ({ product, addToCart, isInCart }) => {
             className="w-10 h-10 object-contain"
           />
         </div>
-      </div>
-
-      {/* NAME */}
+      </div>  
       <h3 className="text-xl font-bold text-[#111827] mb-2">
         {product.name}
       </h3>
-
-      {/* DESCRIPTION */}
       <p className="text-gray-500 text-sm mb-6 leading-relaxed flex-grow">
         {product.description}
-      </p>
-
-      {/* PRICE */}
+      </p>  
       <div className="mb-6 flex items-baseline gap-1">
         <span className="text-3xl font-black text-[#111827]">
           ${product.price}
@@ -51,8 +40,6 @@ const ProductCard = ({ product, addToCart, isInCart }) => {
           /{product.period}
         </span>
       </div>
-
-      {/* FEATURES */}
       <ul className="space-y-4 mb-8">
         {product.features?.map((f, i) => (
           <li key={i} className="flex items-center text-[13px] text-gray-600">
@@ -63,8 +50,6 @@ const ProductCard = ({ product, addToCart, isInCart }) => {
           </li>
         ))}
       </ul>
-
-      {/* BUTTON */}
       <button
         onClick={() => addToCart(product)}
         disabled={isInCart}
@@ -76,7 +61,6 @@ const ProductCard = ({ product, addToCart, isInCart }) => {
       >
         {isInCart ? "✓ Added to Cart" : "Buy Now"}
       </button>
-
     </div>
   );
 };
